a06a991ba5f126691387afaf794590ba79539bff
Odinmk02 <ritarshi.19.10@gmail.com>
2022-03-18 23:24:45 +0530

    Free Sax v2

--- Abc.java
+++ Abc.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 
 public class Abc {
     public static void main(String[] args){
-        int a=1+5435;
+        int a=1+5554535;
         System.out.println(a);
     }
 }
